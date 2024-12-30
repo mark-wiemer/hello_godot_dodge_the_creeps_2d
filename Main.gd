47ed7296eb4c2305842327478fa1134cae32e4a1
Mark Wiemer <markwiemer@outlook.com>
2024-12-29 20:40:04 -0800

    Add death sound and cleanup music

--- Main.gd
+++ Main.gd
@@ -10,6 +10,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
@@ -18,6 +20,7 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.

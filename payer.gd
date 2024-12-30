90f8114d5a199166c22d9e2b272982b3867fed8b
Mark Wiemer <markwiemer@outlook.com>
2024-12-29 19:42:18 -0800

    rename for git...

--- payer.gd
+++ payer.gd
@@ -1,10 +1,17 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -34,3 +41,10 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties in a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
